93f066c06cd3ab69816572fbc05a9c21c9f65099
Adrian Carpenter <adriatic.c@gmail.com>
2020-03-16 03:22:29 -0700

    Theme displays vue site

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,10 @@ module.exports = {
       sass: {prependData: '$NODE_ENV: ' + process.env.NODE_ENV + ';'}
     }
   },
-  outputDir: 'wp-content/themes/bt'
+  // Output looks to wordpress like the index.php of a theme.
+  indexPath: 'index.php',
+  // Place the built site in theme location
+  outputDir: 'wp-content/themes/bt',
+  // Make file paths relative to deploy path; router should have a different configuration
+  publicPath: 'wp-content/themes/bt'
 }
