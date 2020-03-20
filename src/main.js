0efcc78e3bec23997c429de8ee5982307f15e9f4
Adrian Carpenter <adriatic.c@gmail.com>
2020-03-20 16:46:57 -0700

    Consolidate router.js w/main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,53 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
 
-Vue.config.productionTip = false;
+import Router from 'vue-router'
+import Showdown from 'showdown'
+import ShowdownTargetBlank from 'showdown-target-blank'
+
+import App from './App.vue'
+import Home from './views/Home.vue';
+
+
+const showdown = new Showdown.Converter({
+  extensions: [ShowdownTargetBlank]
+})
+
+
+const router = new Router({
+  mode: 'history',
+  base: '/',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+  ],
+})
+
+
+Vue.config.productionTip = false
+
+
+Vue.use(Router)
+
+
+Vue.mixin({
+  methods: {
+    markdown(string) {
+      return showdown.makeHtml(string)
+    },
+  },
+})
+
 
 new Vue({
   router,
   render: h => h(App),
-}).$mount('#app');
+}).$mount('#app')
+
