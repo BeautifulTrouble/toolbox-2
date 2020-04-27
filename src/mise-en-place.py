0311c97cddcb356adf0493991d3ff56b231cfeec
Adrian Carpenter <adriatic.c@gmail.com>
2020-04-26 22:45:45 -0700

    Update bootstrapping script

--- src/mise-en-place.py
+++ src/mise-en-place.py
@@ -3,19 +3,38 @@
 Corners are being cut to keep the site fast.
 '''
 
-# Output tag translations
-
 import json 
 import requests
 
+LANGS = 'en es pt ar'.split()
+
+
+print("Generating tags translations: tags.json")
 TAGS = 'https://api.beautifulrising.org/api/v1/text/tags'
 
-base = requests.get(TAGS).json()
-tags_by_lang = {
-    **{base['lang']: base['all']}, 
-    **{lang: requests.get(f'{TAGS}?lang={lang}').json()['all'] 
-        for lang in base['langs-available'] if lang != base['lang']}
-}
+tags_by_lang = {lang: requests.get(f'{TAGS}?lang={lang}').json()['all'] for lang in LANGS}
 
 with open('tags.json', 'w') as f:
-    json.dump(tags_by_lang, f)
+    json.dump(tags_by_lang, f, indent=2)
+
+
+
+print("Generating types translations: types.json")
+UI = 'https://api.beautifulrising.org/api/v1/text/ui'
+
+types_by_lang = {}
+for lang in LANGS:
+    text = requests.get(f'{UI}?lang={lang}').json()
+    types_by_lang[lang] = {}
+    for one, many in [
+        ('story', 'stories'),
+        ('tactic', 'tactics'),
+        ('principle', 'principles'),
+        ('theory', 'theories'),
+        ('methodology', 'methodologies'),
+        ]:
+        types_by_lang[lang][one] = [text['types'][one], text['types'][many]]
+
+with open('types.json', 'w') as f:
+    json.dump(types_by_lang, f, indent=2)
+    
