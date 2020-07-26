a107dd9db86d3b80d8ac04d533f7dec6c17eff51
Adrian Carpenter <adriatic.c@gmail.com>
2020-07-25 17:38:22 -0700

    Add yet another placeholder string translation file

--- src/mise-en-place.py
+++ src/mise-en-place.py
@@ -40,16 +40,23 @@ with open('types.json', 'w') as f:
     
 
 
-print("Generating type description translations: descriptions.json")
+print("Generating type description translations and 'key-module' strings: descriptions.json, keys.json")
 UI = 'https://api.beautifulrising.org/api/v1/text/ui'
 
 descs = {}
+keys = {}
 for lang in LANGS:
     text = requests.get(f'{UI}?lang={lang}').json()
     descs[lang] = {}
     for T in ['story', 'tactic', 'principle', 'theory', 'methodology']:
         descs[lang][T] = text['definitions'][f'{T}-short']
 
+    keys[lang] = {k: text['module'][k] for k in ['key-principle', 'key-principles', 'key-theory', 'key-theories', 'key-tactic', 'key-tactics', 'key-methodology', 'key-methodologies']}
+
 with open('descriptions.json', 'w') as f:
     json.dump(descs, f, indent=2)
+
+with open('keys.json', 'w') as f:
+    json.dump(keys, f, indent=2)
+
     
