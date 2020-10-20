d78eba2af38a8706680f4628295e96564edc57b9
Adrian Carpenter <adriatic.c@gmail.com>
2020-10-20 10:12:59 -0700

    Vue modifications for WP-free beta

--- public/functions.php
+++ public/functions.php
@@ -1,15 +1,19 @@
 <?php
-// Pass a nonce to the front-end which can be used to fetch draft previews
-function bt_preview_post_link($link) {
-  return $link . '&_wpnonce=' . wp_create_nonce('wp_rest');
+// Theme support
+function bt_setup() {
+    register_nav_menus([
+        'main-menu' => __('Main menu', 'bt'),
+        'secondary-menu' => __('Secondary menu', 'bt'),
+        'footer-menu' => __('Footer menu', 'bt'),
+    ]);
 }
-add_filter('preview_post_link', 'bt_preview_post_link');
+add_action('after_setup_theme', 'bt_setup');
 
 
 // Expose menu items via REST
-// TODO: Does this work?
-// register_post_type('nav_menu_item', array('show_in_rest' => true));
 function bt_get_menus() {
+    // TODO: Does the following line work?
+    // register_post_type('nav_menu_item', array('show_in_rest' => true));
     return [
         wp_get_nav_menu_items('main-menu'), 
         wp_get_nav_menu_items('secondary-menu'),
@@ -22,3 +26,14 @@ add_action('rest_api_init', function() {
         'callback' => 'bt_get_menus',
     ]);
 });
+
+
+// Pass a nonce to the front-end which can be used to fetch draft previews
+function bt_preview_post_link($link) {
+  return $link . '&_wpnonce=' . wp_create_nonce('wp_rest');
+}
+add_filter('preview_post_link', 'bt_preview_post_link');
+
+
+// Expose per-languge posts 
+
--- vue.config.js
+++ vue.config.js
@@ -19,9 +19,11 @@ module.exports = {
     }
   },
   // Output looks to wordpress like the index.php of a theme.
-  indexPath: process.env.NODE_ENV == 'production' ? 'index.php' : 'index.html',
+  //indexPath: process.env.NODE_ENV == 'production' ? 'index.php' : 'index.html',
+  indexPath: 'index.html',
   // Make file paths relative to deploy path; router should have a different configuration
-  publicPath: process.env.NODE_ENV == 'production' ? 'wp-content/themes/bt' : '/',
+  //publicPath: process.env.NODE_ENV == 'production' ? 'wp-content/themes/bt' : '/',
+  publicPath: '/',
   // Place the built site in theme location
   outputDir: 'bt'
 }
