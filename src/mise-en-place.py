255e9425b21ff6179c87b952dc10af5510f17e4c
Adrian Carpenter <adriatic.c@gmail.com>
2021-01-08 09:38:08 -0800

    Add sets

--- src/mise-en-place.py
+++ src/mise-en-place.py
@@ -6,7 +6,7 @@ Corners are being cut to keep the site fast.
 import json 
 import requests
 
-LANGS = 'en es pt ar'.split()
+LANGS = 'en es pt ar fr'.split()
 
 
 print("Generating tags translations: tags.json")
@@ -60,3 +60,22 @@ with open('keys.json', 'w') as f:
     json.dump(keys, f, indent=2)
 
     
+
+print('Generating sets.json')
+sets = {
+    'New Pan-Afrikanism Toolkit': [
+        'citizens-arrest', 'cultural-disobedience', 'hunger-strike', 'lamentation', 'escalate-strategically', 'expose-inequality-with-a-viral-gesture', 'give-voice-to-those-that-cant-speak', 'solidarity-not-aid', 'the-threat-is-usually-more-terrifying-than-the-thing-itself', 'use-organizing-strategies-that-scale', 'gerontocracy', 'palace-coup', 'new-pan-afrikanism', 'kisangani-demands-electric-power', 'angola-15-2', 'bring-back-our-girls', 'fees-must-fall', 'gambia-has-decided', 'ghana-thinktank', 'manich-msamah', 'street-graduation', 'this-flag', 'baraza'
+    ],
+    'COVID-19': [
+        'make-the-invisible-visible', 'simple-rules-can-have-grand-results', 'al-fazaa', 'guerilla-projection', 'divestment', 'take-risks-but-take-care', 'prefigurative-politics', 'peoples-shock-doctrine-', 'breakfast-is-persuasive'
+    ],
+    '#BlackLivesMatter': [
+        'direct-action', 'peoples-shock-doctrine-', 'the-real-action-is-your-targets-reaction', 'anger-works-best-when-you-have-the-moral-high-ground', 'use-the-power-of-ritual', 'hunger-strike', 'countering-homophobic-policing', 'take-risks-but-take-care', 'make-the-invisible-visible', 'do-the-medias-work-for-them', 'power-mapping', 'take-leadership-from-the-most-impacted'
+    ],
+    'Election Protection': [
+        'recapture-the-flag', 'pillars-of-power', 'general-strike', 'put-your-target-in-a-decision-dilemma', 'use-the-power-of-ritual', 'training-for-the-win', 'gambia-has-decided'
+    ],
+}
+with open('sets.json', 'w') as f:
+    json.dump(sets, f, indent=2)
+
